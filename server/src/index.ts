dae7ff947a99672bc57a59e31343be3dc58270e3
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-18 08:53:52 +0530

    Latest server

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,14 +12,14 @@ let allSockets: User[] = [];
 wss.on("connection", (socket) => {
   socket.on('message', (message) => {
     const parsedMessage = JSON.parse(message as unknown as string)
-    if(parsedMessage.type === 'join'){
+    if(parsedMessage.type == "join"){
       allSockets.push({
         socket,
         room: parsedMessage.payload.roomId
       })
     }
 
-    if(parsedMessage.type === "chat"){
+    if(parsedMessage.type == "chat"){
       let currUserRoom = null
       for(let i = 0; i < allSockets.length; i++){
         if(allSockets[i].socket === socket){
