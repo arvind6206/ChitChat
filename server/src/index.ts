f3856ff6120157a6daee3d725efcadcd9cf88cc8
arvind6206 <vishwkarmaarvind1222@gmail.com>
2026-07-18 08:36:35 +0530

    Latest

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,21 +2,39 @@ import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User{
+  socket: WebSocket;
+  room: string
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-
-  userCount += 1;
-
-  console.log("user connected #" + userCount);
-
-  socket.on("message", (msg) => {
-    console.log("message received " + msg.toString());
-
-    for (const s of allSockets) {
-      s.send(msg.toString() + ": sent from server");
+  socket.on('message', (message) => {
+    const parsedMessage = JSON.parse(message as unknown as string)
+    if(parsedMessage.type === 'join'){
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId
+      })
     }
+
+    if(parsedMessage.type === "chat"){
+      let currUserRoom = null
+      for(let i = 0; i < allSockets.length; i++){
+        if(allSockets[i].socket === socket){
+          currUserRoom = allSockets[i].room
+        }
+      }
+
+      for(let i = 0; i < allSockets.length; i++){
+        if(allSockets[i].room === currUserRoom){
+          allSockets[i]?.socket.send(parsedMessage.payload.message)
+        }
+      }
+
+    }
+  })
+
+ 
   });
-});
